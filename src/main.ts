db868aa3279181ae7c16d2c8c04be97b3aa11a67
akshitagarg275 <akshitagarg275@gmail.com>
2022-08-23 10:26:05 +0530

    final code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module'
 
-if (environment.production) {
-  enableProdMode();
-}
-
+// need to provide bootstrapping file which is the initial file
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
